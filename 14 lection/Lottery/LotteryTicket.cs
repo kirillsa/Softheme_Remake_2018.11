794e0c6fb82a850482a07282a652664d6e4fb0a8
Kirill Savitskiy <kirillsavik@gmail.com>
2018-12-07 19:00:26 +0200

    task14

---
+++ 14 lection/Lottery/LotteryTicket.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    class LotteryTicket
+    {
+        private int[] ticket;
+
+        public LotteryTicket()
+        {
+            ticket = new int[6];
+        }
+
+        public int this[int i]
+        {
+            get { return ticket[i]; }
+            private set { ticket[i] = value; }
+        }
+
+        public void InputTicketNumbers()
+        {
+            int bufNumber = 0;
+            for (int i = 0; i < ticket.Length; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Please enter {i + 1} number. It should be between 1 and 9");
+                    int.TryParse(Console.ReadLine(), out bufNumber);
+                } while (bufNumber < 1 || bufNumber > 9);
+
+                this[i] = bufNumber;
+            }
+        }
+
+        private int[] Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        var buf = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = buf;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        public bool Compare(int[] arr)
+        {
+            ticket = Sort(ticket);
+            arr = Sort(arr);
+            for (int i = 0; i < ticket.Length; i++)
+            {
+                if (ticket[i] != arr[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ 14 lection/Lottery/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    class Program
+    {
+        static int[] RandomTicket(int n)
+        {
+            var random = new Random();
+            var arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(1, 10);
+            }
+
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            var lotteryTicket = new LotteryTicket();
+            var randomTicket = RandomTicket(6);
+            Console.WriteLine("This is a lottery. \nYou have to write down 6 numbers between 1 and 9\nTo continue press Enter");
+            Console.ReadLine();
+            Console.Clear();
+            lotteryTicket.InputTicketNumbers();
+            if (lotteryTicket.Compare(randomTicket))
+            {
+                Console.WriteLine("You guess all numbers");
+            }
+            else
+            {
+                Console.WriteLine("You lost");
+            }
+            Console.WriteLine("Your ticket    Winner ticket");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(lotteryTicket[i] + "              " + randomTicket[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
