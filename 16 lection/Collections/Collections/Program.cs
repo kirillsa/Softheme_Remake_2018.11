13c162ecb44fadde62234518c74264c2f6e2f300
Kirill Savitskiy <kirillsavik@gmail.com>
2018-12-14 12:55:47 +0200

    task16

---
+++ 16 lection/Collections/Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>(4);
+            stack.Pop();
+            stack.Peek();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.ShowStack();
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.ShowStack();
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.ShowStack();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 16 lection/Collections/Collections/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Stack <T>
+    {
+        private T[] arr;
+        private int current;
+
+        public Stack (int size)
+        {
+            arr = new T[size];
+            current = -1;
+        }
+
+        public void Push (T value)
+        {
+            if (current == arr.Length-1)
+            {
+                Console.WriteLine("Stack overflow");
+                return;
+            }
+
+            arr[++current] = value;
+        }
+
+        public T Pop()
+        {
+            if (current == -1)
+            {
+                Console.WriteLine("Stack is empty. First Push something");
+                return arr[0];
+            }
+            else
+            {
+                return arr[current--];
+            }
+        }
+
+        public T Peek()
+        {
+            if (current == -1)
+            {
+                Console.WriteLine("Stack is empty. First Push something");
+                return arr[0];
+            }
+
+            return arr[current];
+        }
+
+        public void ShowStack()
+        {
+            if (current == -1)
+            {
+                Console.WriteLine("Stack is empty. First Push something");
+                return;
+            }
+            else
+            {
+                for (var i = 0; i <= current; i++)
+                {
+                    Console.Write(arr[i].ToString() + ' ');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
